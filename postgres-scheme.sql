ca51c2d92a15b4a72af7a2444563d09fc910d79c
Tobias Hartwich <mail@tha.io>
2016-04-26 15:38:47 +0200

    updated pg scheme

--- postgres-scheme.sql
+++ postgres-scheme.sql
@@ -9,3 +9,17 @@ CREATE TABLE uploads (
     content_type character varying,
     slot_time timestamp with time zone
 );
+
+
+CREATE SEQUENCE uploads_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE uploads_id_seq OWNED BY uploads.id;
+
+ALTER TABLE ONLY uploads
+    ADD CONSTRAINT id PRIMARY KEY (id);
